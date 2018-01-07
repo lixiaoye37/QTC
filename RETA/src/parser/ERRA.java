0d30fad55f7fe86ebd4001da98f66cacece7723c
icyfillup <phil10tran@gmail.com>
2018-01-07 05:12:05 -0800

    ERRA can now be use proper in the RETA system

--- RETA/src/parser/ERRA.java
+++ RETA/src/parser/ERRA.java
@@ -1,9 +1,11 @@
 package parser;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DateUtil;
@@ -11,190 +13,213 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class ERRA
-{
-	public class LocationHistory
-	{
-		String VARO;
-		String State;
-
-		ArrayList<Boolean> QtcAssignedSlot;
-
-		public LocationHistory(String VARO, String State, ArrayList<Boolean> QtcAssignedSlot)
-		{
-			this.VARO = VARO;
-			this.State = State;
-			this.QtcAssignedSlot = QtcAssignedSlot;
-		}
-	}
-	
-	public class Date_Time {
-		String Date;
-		String TimeOfday;
-		
-		public Date_Time(String Date)
-		{
-			this.Date = Date;
-		}
-		
-		public Date_Time(String Date, String TimeOfDay)
-		{
-			this.Date = Date;
-			this.TimeOfday = TimeOfDay;
-		}
-	}
-
-	private ArrayList<LocationHistory> locationHistory;
-	private ArrayList<Date_Time> date_time;
+public class ERRA {
+	private HashMap<String, ArrayList<CityInfo>> TimeSchedule;
 
 	@SuppressWarnings("deprecation")
-	public ERRA(String FilePath) throws IOException
-	{
-		date_time = new ArrayList<>();
-		locationHistory = new ArrayList<>();
+	public ERRA(String FilePath) throws IOException {
+		TimeSchedule = new HashMap<String, ArrayList<CityInfo>>();
+
+		ArrayList<Date_Time> date_time = new ArrayList<>();
+		ArrayList<LocationHistory> locationHistory = new ArrayList<>();
 
 		File myFile = new File(FilePath);
 		FileInputStream fis = new FileInputStream(myFile);
 
 		// Finds the workbook instance for XLSX file
 		XSSFWorkbook myWorkBook = new XSSFWorkbook(fis);
-		
+
 		// Return first sheet from the XLSX workbook
 		XSSFSheet mySheet = myWorkBook.getSheetAt(0);
 
 		// Traversing over each row of XLSX file
 		int RowIndex = 0;
-		for(Row row : mySheet)
-		{
-			if(RowIndex > 2)
-			{
+		for (Row row : mySheet) {
+			if (RowIndex > 2) {
 				String VARO = null;
 				String State = null;
 				int indexTracker = 0;
 				ArrayList<Boolean> QtcAssignedSlot = new ArrayList<>();
 
-				for(Cell cell : row)
-				{
-					if(indexTracker > 1)
-					{
+				for (Cell cell : row) {
+					if (indexTracker > 1) {
 						boolean isQtc = (cell.getStringCellValue().toUpperCase().contains("QTC")) ? true : false;
 						QtcAssignedSlot.add(isQtc);
-					}
-					else if(indexTracker == 0)
-					{
+					} else if (indexTracker == 0) {
 						VARO = cell.getStringCellValue();
-					}
-					else if(indexTracker == 1)
-					{
+					} else if (indexTracker == 1) {
 						State = cell.getStringCellValue();
 					}
 					indexTracker++;
 				}
 
-				if(!(VARO.isEmpty() || State.isEmpty()))
-				{
-					this.locationHistory.add(new LocationHistory(VARO, State, QtcAssignedSlot));
+				if (!(VARO.isEmpty() || State.isEmpty())) {
+					locationHistory.add(new LocationHistory(VARO, State, QtcAssignedSlot));
 				}
-			}
-			else if(RowIndex == 1)
-			{
+			} else if (RowIndex == 1) {
 				int SkipFirstTwo = 0;
-				for(Cell cell : row)
-				{
-					if(SkipFirstTwo < 2)
-					{
+				for (Cell cell : row) {
+					if (SkipFirstTwo < 2) {
 						SkipFirstTwo++;
 						continue;
 					}
 
-					switch(cell.getCellType())
-					{
-						case Cell.CELL_TYPE_STRING:
-						{
-							if(cell.getStringCellValue().contains("AM") || cell.getStringCellValue().contains("PM"))
-							{
-								String dateWithNoSpaces = cell.getStringCellValue().replaceAll(" ", "");
-								int TimeOfDayIndex = (dateWithNoSpaces.length() - 1) - 1;
-								String date = dateWithNoSpaces.substring(0, TimeOfDayIndex);
-								String TimeOfDay = dateWithNoSpaces.substring(TimeOfDayIndex,
-										dateWithNoSpaces.length());
-								getDate_time().add(new Date_Time(date, TimeOfDay));
-							}
-							else
-							{
-								System.out.println(
-										"MainActivity.java: error in switch case [Cell.CELL_TYPE_STRING]: not suppose to enter this branch. formatting issue");
-								System.exit(1);
-							}
+					switch (cell.getCellType()) {
+					case Cell.CELL_TYPE_STRING: {
+						if (cell.getStringCellValue().contains("AM") || cell.getStringCellValue().contains("PM")) {
+							String dateWithNoSpaces = cell.getStringCellValue().replaceAll(" ", "");
+							int TimeOfDayIndex = (dateWithNoSpaces.length() - 1) - 1;
+							String date = dateWithNoSpaces.substring(0, TimeOfDayIndex);
+							String TimeOfDay = dateWithNoSpaces.substring(TimeOfDayIndex, dateWithNoSpaces.length());
+							date_time.add(new Date_Time(date, TimeOfDay));
+						} else {
+							System.out.println(
+									"MainActivity.java: error in switch case [Cell.CELL_TYPE_STRING]: not suppose to enter this branch. formatting issue");
+							System.exit(1);
 						}
+					}
 						break;
 
-						case Cell.CELL_TYPE_NUMERIC:
-						{
-							if(DateUtil.isCellDateFormatted(cell))
-							{
-								SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-								getDate_time().add(new Date_Time(dateFormat.format(cell.getDateCellValue())));
-							}
-							else
-							{
-								System.out.println(
-										"MainActivity.java: error in switch case [Cell.CELL_TYPE_NUMERIC]: not suppose to enter this branch. formatting issue");
-								System.exit(1);
-							}
+					case Cell.CELL_TYPE_NUMERIC: {
+						if (DateUtil.isCellDateFormatted(cell)) {
+							SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+							date_time.add(new Date_Time(dateFormat.format(cell.getDateCellValue())));
+						} else {
+							System.out.println(
+									"MainActivity.java: error in switch case [Cell.CELL_TYPE_NUMERIC]: not suppose to enter this branch. formatting issue");
+							System.exit(1);
 						}
+					}
 						break;
 
-						case Cell.CELL_TYPE_BLANK:
-						{
-							getDate_time().add(new Date_Time(getDate_time().get(getDate_time().size() - 1).Date));
-						}
+					case Cell.CELL_TYPE_BLANK: {
+						date_time.add(new Date_Time(date_time.get(date_time.size() - 1).Date));
+					}
 						break;
 					}
 				}
-			}
-			else if(RowIndex == 2)
-			{
+			} else if (RowIndex == 2) {
 				int SkipFirstTwo = 0;
 				int indexTracker = 0;
 
-				for(Cell cell : row)
-				{
-					if(SkipFirstTwo < 2)
-					{
+				for (Cell cell : row) {
+					if (SkipFirstTwo < 2) {
 						SkipFirstTwo++;
 						continue;
 					}
 
-					if(cell.getCellType() == Cell.CELL_TYPE_STRING)
-					{
-						getDate_time().get(indexTracker).TimeOfday = cell.getStringCellValue();
+					if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
+						date_time.get(indexTracker).TimeOfDay = cell.getStringCellValue();
 					}
 					indexTracker++;
 				}
 			}
 			RowIndex++;
 		}
+
+		for (int i = 0; i < date_time.size(); i++) {
+			Date_Time dateTime = date_time.get(i);
+			String[] MonthDayYear = dateTime.Date.split("/");
+
+			if (MonthDayYear[0].length() == 1) {
+				MonthDayYear[0] = "0" + MonthDayYear[0];
+			}
+
+			if (MonthDayYear[1].length() == 1) {
+				MonthDayYear[1] = "0" + MonthDayYear[1];
+			}
+
+			if (MonthDayYear[2].length() == 2) {
+				MonthDayYear[2] = "20" + MonthDayYear[2];
+			}
+
+			String date = MonthDayYear[0] + "/" + MonthDayYear[1] + "/" + MonthDayYear[2];
+			ArrayList<CityInfo> ListOfCities = new ArrayList<>();
+
+			if (TimeSchedule.containsKey(date)) {
+				ListOfCities = TimeSchedule.get(date);
+
+				for (int LocationIndex = 0; LocationIndex < locationHistory.size(); LocationIndex++) {
+					LocationHistory location = locationHistory.get(LocationIndex);
+					CityInfo City = ListOfCities.get(LocationIndex);
+					boolean isQtcAssigned = location.QtcAssignedSlot.get(i);
+
+					if (isQtcAssigned == true) {
+						City.IsQtcAssignedSlot = isQtcAssigned;
+					}
+				}
+			} else {
+				ListOfCities = new ArrayList<>();
+				for (LocationHistory location : locationHistory) {
+					String VARO = location.VARO;
+					String State = location.State;
+
+					boolean isQtcAssigned = location.QtcAssignedSlot.get(i);
+
+					ListOfCities.add(new CityInfo(VARO, State, isQtcAssigned));
+				}
+				TimeSchedule.put(date, ListOfCities);
+			}
+
+		}
+
 		myWorkBook.close();
 	}
 
-	public ArrayList<LocationHistory> getLocationHistory() {
-		return locationHistory; 
+	public class CityInfo {
+		private String VARO;
+		private String State;
+		private boolean IsQtcAssignedSlot;
+
+		public CityInfo(String varo, String state, boolean isQtcAssignedSlot) {
+			super();
+			VARO = varo;
+			State = state;
+			IsQtcAssignedSlot = isQtcAssignedSlot;
+		}
+
+		public String getVARO() {
+			return VARO;
+		}
+
+		public String getState() {
+			return State;
+		}
+
+		public boolean isIsQtcAssignedSlot() {
+			return IsQtcAssignedSlot;
+		}
 	}
 
-	public void setLocationHistory(ArrayList<LocationHistory> locationHistory) {
-		this.locationHistory = locationHistory;
+	private class LocationHistory {
+		private String VARO;
+		private String State;
+
+		private ArrayList<Boolean> QtcAssignedSlot;
+
+		private LocationHistory(String VARO, String State, ArrayList<Boolean> QtcAssignedSlot) {
+			this.VARO = VARO;
+			this.State = State;
+			this.QtcAssignedSlot = QtcAssignedSlot;
+		}
+	}
+	
+	public HashMap<String, ArrayList<CityInfo>> getTimeSchedule() {
+		return TimeSchedule;
 	}
 
-	public ArrayList<Date_Time> getDate_time() {
-		return date_time;
+	private class Date_Time {
+		private String Date;
+		private String TimeOfDay;
+
+		private Date_Time(String Date) {
+			this.Date = Date;
+			this.TimeOfDay = null;
+		}
+
+		private Date_Time(String Date, String TimeOfDay) {
+			this.Date = Date;
+			this.TimeOfDay = TimeOfDay;
+		}
 	}
-
-	public void setDate_time(ArrayList<Date_Time> date_time) {
-		this.date_time = date_time;
-	}
-
-
-
-
 }
